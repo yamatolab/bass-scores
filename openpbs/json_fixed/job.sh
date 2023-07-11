e0a9364c40c8da030255bed78b4a0879f2d0be49
Wataru Sugiura <flat35hd99@gmail.com>
2023-07-12 00:36:58 +0900

    Fix openpbs build

--- openpbs/json_fixed/job.sh
+++ openpbs/json_fixed/job.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -eu
-#PBS -l select=1:ncpus=12
+#PBS -l select=1:ncpus=4
 #PBS -l walltime=24:00:00
 
 GIT_REF=json_leading_zeros
 
-PREFIX=/apl/packages/util/openpbs/$GIT_REF
+PREFIX=$PREFIX_APP_DIR/packages/util/openpbs/$GIT_REF
 
-cd $PBS_O_WORKDIR
-git clone https://github.com/CESNET/pbspro.git
+cd /lwork/users/$USER/$PBS_JOBID
+git clone https://github.com/flat35hd99/pbspro.git
 cd pbspro
 git checkout $GIT_REF 
 
 ./autogen.sh
 ./configure --prefix=${PREFIX}
-make -j12
+make -j `nproc`
 make install
 
