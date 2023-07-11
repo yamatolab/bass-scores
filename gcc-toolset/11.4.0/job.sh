3c7c54fc864d979b2eec1d5ec90cda3cff61e3ae
Wataru Sugiura <flat35hd99@gmail.com>
2023-07-12 00:50:32 +0900

    Fix gcc-tool

--- gcc-toolset/11.4.0/job.sh
+++ gcc-toolset/11.4.0/job.sh
@@ -4,28 +4,32 @@
 
 ## Build and install
 ## =================
-VERSION="11.4.0"
-PACKAGE_PREFIX=/apl/packages/util/gcc-toolset/$VERSION
+software_kind=util
+name=gcc-toolset
+version="11.4.0"
+
+PREFIX=$PREFIX_APP_DIR/packages/$software_kind/$name/$version
 
 WORK_DIR=/lwork/users/$USER/$PBS_JOBID
-mkdir -p $WORK_DIR
 cd $WORK_DIR
 
-wget "http://ftp.tsukuba.wide.ad.jp/software/gcc/releases/gcc-${VERSION}/gcc-${VERSION}.tar.xz"
-tar -xvf "gcc-${VERSION}.tar.xz"
+# Download source
+wget "http://ftp.tsukuba.wide.ad.jp/software/gcc/releases/gcc-${version}/gcc-${version}.tar.xz"
+tar -xvf "gcc-${version}.tar.xz"
 
+# Build
 mkdir objdir
 cd objdir
-$PWD/../"gcc-${VERSION}"/configure \
-    --prefix=${PACKAGE_PREFIX} \
+$PWD/../"gcc-${version}"/configure \
+    --prefix=$PREFIX \
     --enable-languages=c,c++,fortran \
     --disable-multilib
 make -j `nproc`
 make install
 
-## Add module file
-## ===============
-mkdir -p /apl/modules/util/gcc-toolset
-cat $TEMPLATE_MODULEFILE \
-    | sed -e "s/TEMPLATE_PREFIX/${PACKAGE_PREFIX}/g" 
-        > /apl/modules/util/gcc-toolset/${VERSION}
+# Create modulefile
+module_file_path=$PREFIX_APP_DIR/modules/$software_kind/$name/$version
+mkdir -p `dirname $module_file_path`
+# Set template variables
+export TEMPLATE_PREFIX=$PREFIX
+envsubst < $APPLICATION_REPOSITORY_PATH/$name/template_modulefile > $module_file_path
