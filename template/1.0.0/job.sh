38408abae5c37d6f173408a4b9cc077389755e3e
Wataru Sugiura <flat35hd99@gmail.com>
2023-07-12 18:00:53 +0900

    Add fix permission commands

--- template/1.0.0/job.sh
+++ template/1.0.0/job.sh
@@ -26,3 +26,8 @@ mkdir -p `dirname $module_file_path`
 # Commands below assume that TEMPLATE_PREFIX is used in template_modulefile
 export TEMPLATE_PREFIX=$PREFIX
 envsubst < $APPLICATION_REPOSITORY_PATH/$name/template_modulefile > $module_file_path
+
+## Ensure all users have executable permission for all directories
+## and read permission for all files
+find $PREFIX_APP_DIR/modules/$software_kind/$name -type d -exec chmod 755 {} \;
+chmod 644 $PREFIX_APP_DIR/modules/$software_kind/$name/$version
