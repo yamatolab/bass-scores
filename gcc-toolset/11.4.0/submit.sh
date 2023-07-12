f46ede5899bfbe9647e364aff99f967c174673b2
Wataru Sugiura <flat35hd99@gmail.com>
2023-07-12 16:19:42 +0900

    Refactor of submitter of gcc-toolset

--- gcc-toolset/11.4.0/submit.sh
+++ gcc-toolset/11.4.0/submit.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-qsub -N gcc-toolset-11.4.0 -v TEMPLATE_MODULEFILE=$PWD/../template_modulefile job.sh
+# Assume that the job.sh is in the same directory as this script
+qsub -N gcc-toolset-11.4.0 `dirname $0`/job.sh
