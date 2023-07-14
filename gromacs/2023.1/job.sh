36308973c883ee8bacd80798fc82d4bf4a2b7f9f
Wataru Sugiura <flat35hd99@gmail.com>
2023-07-14 14:56:39 +0900

    Enable MPI of gromacs

--- gromacs/2023.1/job.sh
+++ gromacs/2023.1/job.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -eu
-#PBS -l select=1:ncpus=4
+#PBS -l select=1:ncpus=12:mpiprocs=8
 #PBS -l walltime=03:00:00
+
+# The test suite of gromacs requires 8 MPI processes.
 
 # Implicit parameters
 #  - PREFIX_APP_DIR (e.g. /apl)
@@ -10,6 +12,9 @@
 software_kind=apl # or util
 name=gromacs
 version=2023.1
+module_loaded="openmpi/4.1.5"
+
+module load $module_loaded
 
 PREFIX=$PREFIX_APP_DIR/packages/$software_kind/$name/$version
 
@@ -27,6 +32,7 @@ cd build
 cmake .. \
     -DGMX_BUILD_OWN_FFTW=ON \
     -DREGRESSIONTEST_DOWNLOAD=ON \
+    -DGMX_MPI=ON \
     -DCMAKE_INSTALL_PREFIX=$PREFIX
 make -j `nproc`
 # Unset OMP_NUM_THREADS to avoid errors in test.
@@ -42,6 +48,7 @@ module_file_path=$PREFIX_APP_DIR/modules/$software_kind/$name/$version
 mkdir -p `dirname $module_file_path`
 # Set template variables
 export TEMPLATE_PREFIX=$PREFIX
+export TEMPLATE_DEPENDS_ON=$module_loaded
 envsubst < $APPLICATION_REPOSITORY_PATH/$name/template_modulefile > $module_file_path
 
 ## Ensure all users have executable permission for all directories
